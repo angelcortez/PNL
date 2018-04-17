5edda85442ae71750b07f6d024ea7aaf69d0e873
angelcortez <37381401+angelcortez@users.noreply.github.com>
2018-04-17 12:23:37 -0500

    Se actualizaron los archivos pertinentes para la funcionalidad de editar alias

---
+++ SQL/PROCEDIMIENTOS/2018/ABRIL/17 de Abril/consultaAlias.sql
@@ -0,0 +1,21 @@
+﻿--=============================================
+--Author:    <Angel Cortez>
+--Create date: <17 de Abril de 2018>
+--Description:  <Se agregó a la consulta el tipo de actor>
+-- =============================================
+CREATE PROCEDURE [dbo].[consultaAlias] @IdPersona int
+as
+begin
+
+
+	SELECT al.ID_ALIAS   
+	,      ALIAS         
+	,      al.ID_PERSONA 
+	,      actor.ACTR_TPO as tipoActor
+	FROM       PGJ_ALIAS           al   
+	inner join PGJ_CARPETA_PERSONA cp    ON cp.ID_PERSONA= al.ID_PERSONA
+	inner join CAT_ACTOR_TIPO      actor on cp.ID_TIPO_ACTOR = actor.ID_ACTR_TPO
+	where al.ID_PERSONA=@IdPersona
+
+end
+
